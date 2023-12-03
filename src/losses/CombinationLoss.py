bug fixes in loss calculation

src/losses/CombinationLoss.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 from utils.post_processing import *
 
 class CombinationLoss(nn.Module):
-    def __init__(self, loss_function, data_imputation='zeros', threshold=0.5):
+    def __init__(self, loss_function, data_imputation='zeros', diseases=None, threshold=0.5):
         super().__init__()
         self.loss_function = loss_function
         self.data_imputation = data_imputation
         self.threshold = threshold
+        self.diseases = diseases
 
     def forward(self, n_batch_distribution_tensor):
         # Adding a small epsilon to avoid log(0)
@@ -20,7 +21,12 @@ class CombinationLoss(nn.Module):
         # The reference tensor is the tensor of probabilities for each disease combination in the entire dataset
         # The reference tensor is of shape (num_combinations)
 
-        target_tensor, ref_tensor = post_process(n_batch_distribution_tensor, self.data_imputation, self.threshold)
+        accumulated_outputs = torch.sigmoid(n_batch_distribution_tensor)
+
+        target, ref = post_process(accumulated_outputs, self.data_imputation, self.diseases, self.threshold)
+        target_tensor = target['distributions']
+        ref_tensor = ref['distributions']
+        
         if self.loss_function == 'KLDivergence':
             return self.KL_divergence(target_tensor, ref_tensor)
         elif self.loss_function == 'CosineSimilarity':
@@ -39,9 +45,8 @@ class CombinationLoss(nn.Module):
     def KL_divergence(self, avg_tensor, ref_tensor):
         # Adding a small epsilon to avoid log(0)
         epsilon = 1e-10
-        print(avg_tensor)
-        avg_tensor = avg_tensor # + epsilon
-        ref_tensor = ref_tensor # + epsilon
+        avg_tensor = avg_tensor + epsilon
+        ref_tensor = ref_tensor + epsilon
         return torch.sum(ref_tensor * torch.log(ref_tensor / avg_tensor))
 
     def cosine_similarity(self, avg_tensor, ref_tensor):

src/modules/ImageComponentModule.py
@@ -32,10 +32,10 @@ class ImageComponentModule(pl.LightningModule):
         self.accumulated_steps =  opt['model']["accumulated_batches"]
         self.threshold =  opt['model']["threshold"]
         self.data_imputation =  opt['dataset']["data_imputation"]
-        self.current_outputs = []
-        self.current_labels = []
+        self.diseases = opt['dataset']['chexpert_labels']
+        self.accumulated_outputs = []
         self.image_encoder_model = opt['model']["image_encoder_model"]
-        self.criterion = CombinationLoss(self.loss_func, self.data_imputation, self.threshold)
+        self.criterion = CombinationLoss(self.loss_func, self.data_imputation, self.diseases, self.threshold)
         self.model = self._get_model()
 
     def forward(self, x):
@@ -44,23 +44,20 @@ class ImageComponentModule(pl.LightningModule):
         return x
     
     def reset_accumulation(self):
-        self.current_outputs = []
-        self.current_labels = []
+        self.accumulated_outputs = []
 
-    def accumulate_outputs_labels(self, outputs, labels):
-        self.current_outputs.append(outputs)
-        self.current_labels.append(labels)
-
+    def accumulate_outputs(self, outputs):
+        self.accumulated_outputs.append(outputs)
+ 
     def compute_accumulated_loss(self):
-        if not self.current_outputs:
+        if not self.accumulated_outputs:
             # No previous data to compare with
             print("No previous data to compare with")
             return None
 
-        current_outputs = self.current_outputs
-        
+        accumulated_outputs = torch.cat(self.accumulated_outputs, dim=0)
         # Compute loss between current and previous batches
-        loss = self.criterion(current_outputs)
+        loss = self.criterion(accumulated_outputs)
         self.reset_accumulation()
         return loss
 
@@ -84,9 +81,9 @@ class ImageComponentModule(pl.LightningModule):
     
 
     def training_step(self, train_batch, batch_idx):
-        x, labels = train_batch['target'], train_batch['report']
+        x = train_batch['target']
         output = self.forward(x)
-        self.accumulate_outputs_labels(torch.sigmoid(output), labels)
+        self.accumulate_outputs(output)
 
         if (batch_idx + 1) % self.accumulated_steps == 0:
             loss = self.compute_accumulated_loss()
@@ -95,9 +92,9 @@ class ImageComponentModule(pl.LightningModule):
                 return loss
 
     def validation_step(self, val_batch, batch_idx):
-        x, labels = val_batch['target'], val_batch['report']
+        x = val_batch['target']
         output = self.forward(x)
-        self.accumulate_outputs_labels(output, labels)
+        self.accumulate_outputs(output)
 
         if (batch_idx + 1) % self.accumulated_steps == 0:
             loss = self.compute_accumulated_loss()

src/utils/post_processing.py
@@ -8,60 +8,56 @@ from utils.environment_settings import env_settings
 import json
 from utils.utils import read_disease_combination_counts
 
-diseases = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Enlarged Cardiomediastinum',
-            'Fracture', 'Lung Lesion', 'Lung Opacity', 'No Finding', 'Pleural Effusion', 'Pleural Other',
-            'Pneumonia', 'Pneumothorax']
-
-
 overall_probabilities = {
     "zeros" : read_disease_combination_counts(path=env_settings.OCCURENCE_PROBABILITIES['zeros']),
     "ones" : read_disease_combination_counts(path=env_settings.OCCURENCE_PROBABILITIES['ones'])
 }
 
-def post_process(labels_arr, data_imputation, threshold=0.5):
+def post_process(accumulated_batches, data_imputation, diseases, threshold=0.5):
     # Convert the model output probabilities into binary presence (0 or 1) based on the threshold
 
-    n_batch_distribution = []
-    n_batch_distribution_tensor = []
-    n_reference_distribution_tensor = []
-    n_reference_distribution = []
-    overall_distirbution = overall_probabilities[data_imputation]
-    print(len(labels_arr))
-
-    for label in labels_arr:
-        presence = (label > threshold).int()
+    combinations_in_batches = []
+    output_probabilities = {}
+    reference_probabilities = {}
+    
+    for output in accumulated_batches:
+        presence = (output > 0.5).int()
         # Generate disease combinations as strings based on model output
-        combinations_in_batch = []
-        for batch in presence:
-            diseases_present = [diseases[idx] for idx, present in enumerate(batch) if present == 1]
-            if not diseases_present:
-                diseases_present = ['No Finding']
-            disease_combination = ', '.join(sorted(diseases_present))
-            combinations_in_batch.append(disease_combination)
+        output_combination = [diseases[idx] for idx in range(len(output)) if presence[idx] == 1]
+        if not output_combination:
+            output_combination = ['No Finding']
+        output_combination = ', '.join(sorted(output_combination))
+        combinations_in_batches.append(output_combination)
+
+    for combo in combinations_in_batches:
+        if combo in output_probabilities:
+            output_probabilities[combo] += 1
+        else:
+            output_probabilities[combo] = 1
+
+    for combo in output_probabilities:
+        output_probabilities[combo] /= len(output_probabilities)
+
     
-        # Calculate occurrence probabilities for each combination in the batch
-        batch_distribution = {}
-        for combo in combinations_in_batch:
-            if combo in batch_distribution:
-                batch_distribution[combo] += 1
-            else:
-                batch_distribution[combo] = 1
-        for combo in batch_distribution:
-            batch_distribution[combo] /= len(combinations_in_batch)
+    reference_probabilities = {combo: overall_probabilities[data_imputation].get(combo, 0) for combo in output_probabilities.keys()}
+
+
+    output_distribution = torch.tensor(list(output_probabilities.values()), dtype=torch.float32)
+    reference_distribution = torch.tensor(list(reference_probabilities.values()), dtype=torch.float32)
+
+    output = {
+        'combinations' : output_probabilities,
+        'distributions' : output_distribution
+    }
+
+    reference = {
+        'combinations' : reference_probabilities,
+        'distributions' : reference_distribution
+    }
     
-        # Convert batch_distribution to tensor for loss calculation
-        batch_distribution_tensor = torch.tensor(list(batch_distribution.values()), dtype=torch.float32)
-        n_batch_distribution_tensor.append(batch_distribution_tensor)
-        n_batch_distribution.append(batch_distribution)
-
-        # Extract only the relevant combinations from the entire dataset distribution
-        reference_distribution = {combo: overall_distirbution.get(combo, 0) for combo in batch_distribution.keys()}
-        reference_distribution_tensor = torch.tensor(list(reference_distribution.values()), dtype=torch.float32)
-        n_reference_distribution_tensor.append(reference_distribution_tensor)
-        n_reference_distribution.append(reference_distribution)
-
-    return n_batch_distribution_tensor, n_reference_distribution_tensor
-
+    
+    return output, reference
+        
 
 
 