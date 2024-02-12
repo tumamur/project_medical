e7e9cd3aaa376f1632c7c12969954e8746b323ca
maxiobe <maxi.oberle@web.de>
2024-02-12 13:13:32 +0100

    Switching to img_embedding and not global_embedding

--- src/models/BioViL.py
+++ src/models/BioViL.py
@@ -5,6 +5,7 @@ import pytorch_lightning as pl
 import health_multimodal.image
 from health_multimodal.image.model.model import BaseImageModel
 from health_multimodal.image.utils import ImageModelType
+from health_multimodal.image.model.modules import MultiTaskModel
 from health_multimodal.image.model.pretrained import get_biovil_t_image_encoder, get_biovil_image_encoder
 
 
@@ -16,7 +17,8 @@ class VisionTransformer(nn.Module):
         # print(self.model)
 
     def forward(self, x):
-        x = self.model.forward(x).projected_global_embedding
+        # x = self.model.forward(x).projected_global_embedding
+        x = self.model.forward(x).img_embedding
         # Check if normalization is needed
         x = torch.nn.functional.normalize(x, dim=1)
         return x
@@ -59,4 +61,23 @@ class BioViL(nn.Module):
     def forward(self, x):
         x = self.VisionTransformer(x)
         x = self.ClassificationHead(x)
+        return x
+
+
+class BioViL_V2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_model = get_biovil_t_image_encoder()
+        self.multi_task_classifier = MultiTaskModel(
+            input_dim=512,
+            classifier_hidden_dim=256,
+            num_classes=1,
+            num_tasks=13  # Assuming num_classes = num_tasks for Chexpert
+        )
+
+    def forward(self, x):
+        # Pass input through the image model, which includes the downstream classifier
+        x = self.image_model(x).img_embedding
+        x = self.multi_task_classifier(x)
+        x = x.squeeze(1)  # Specify dimension to squeeze
         return x
