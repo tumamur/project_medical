718fc91fc8ff5a3799165307df31ea10402f13d7
arda.mamur <arda.mamur@tum.de>
2024-02-12 23:56:27 +0100

    experiments defined

--- src/job_submit.sh
+++ src/job_submit.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# version 3 : python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --adaptive_threshold_disc 
-# version 4: python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --adaptive_threshold_gen --adaptive_threshold_disc
-# version : python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --use_float_reports --adaptive_threshold_gen --adaptive_threshold_disc
+# version 1 :
+# python3 --n_epochs 5 --batch_size 16 --soft_label_type gaussian
 
-python3 train.py --n_epoch 50 --batch_size 16 --update_freq 50 --use_float_reports --adaptive_threshold_gen --adaptive_threshold_disc --resume_training --resume_version 2
+# version 2 :
+python3 --n_epochs 5 --batch_size 16
 
+# version 3:
+python3 --n_epochs 5 --batch_size 16 --soft_label_type gaussian --without_decay
 
-python3 train.py --n_epoch 15 --batch_size 1 --update_freq 1000 --adaptive_threshold_gen --adaptive_threshold_disc # version 6: 
-python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --adaptive_threshold_gen --adaptive_threshold_disc  # version 7: 
-python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --adaptive_threshold_gen --adaptive_threshold_disc # version 8: 
-python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --adaptive_threshold_gen --adaptive_threshold_disc --img_gen_lr 0.001 --img_disc_lr 0.00001 --report_gen_lr 0.0001 --report_disc_lr 0.00001 #version 9
-python3 train.py --n_epoch 15 --batch_size 16 --update_freq 50 --adaptive_threshold_gen --adaptive_threshold_disc --lambda_cycle_loss 20 --img_gen_lr 0.001 --img_disc_lr 0.00001 --report_gen_lr 0.0001 --report_disc_lr 0.00001 #version 10
+# version 4:
+python3 --n_epochs 5 --batch_size 16 --without_decay
+
+# version 5;
+python3 --n_epochs 5 --batch_size 16 --use_all_images --soft_label_type gaussian
